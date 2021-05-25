fba440e231c95a954db88e868220f031992a127b
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-05-25 15:48:26 -0400

    1548 5/25

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require( "sequelize" );
 
 const db = new Sequelize( process.env.DATABASE_URL, {
     dialect: 'postgres',
-
+    ssl: process.env.ENVIROMENT === 'production'
 } );
 
 module.exports = db;
