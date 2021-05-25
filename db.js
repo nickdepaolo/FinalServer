7093b5b43b8d163b88df601d6ce2f63fb70281ad
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-05-25 15:51:13 -0400

    any2

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ const { Sequelize } = require( "sequelize" );
 const db = new Sequelize( process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIROMENT === 'production'
-} );
+});
 
 module.exports = db;
